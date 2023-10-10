5657aad9575f6de736854626c31c6534e0046752
donkok <donatas.kokanauskas@teltonika.lt>
2023-10-10 08:33:40 +0300

    vue router

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import PostsPage from "../views/PostsPage.vue";
 import PostsDetailPage from "../views/PostDetailPage.vue";
+import Page404 from "../components/Page404.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
     { path: "/", component: PostsPage },
     { path: "/postsDetailPage", component: PostsDetailPage },
+    { path: "*", component: Page404 },
 ];
 
 const router = new VueRouter({

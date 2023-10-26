1fde69e18e632d82f7e47dde1b7071a077726676
donkok <donatas.kokanauskas@teltonika.lt>
2023-10-26 13:43:34 +0300

    getter delete

--- src/views/PostsPage.vue
+++ src/views/PostsPage.vue
@@ -35,7 +35,7 @@ export default {
         Pagination,
     },
     computed: {
-        ...mapGetters(["allArticles", "visibleArticles"]),
+        ...mapGetters(["allArticles"]),
     },
     methods: {
         ...mapActions([
